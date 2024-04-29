cb62d24d0ce6c5ea361545c2ad402ef5e9e4ec8b
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-04-29 16:44:08 +0900

    Initial init
     Initial init

---
+++ users/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from .models import User
+
+class TinyUserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = (
+            "name",
+            "avatar",
+            "username",
+        )
+
+class PrivateUserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        exclude = (
+            "password",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "id",
+            "groups",
+            "user_permissions",
+        )
+        
+class PublicUserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "is_host",
+            "name",
+            "avatar",
+            "gender",
+            "language",
+            "currency",
+        )

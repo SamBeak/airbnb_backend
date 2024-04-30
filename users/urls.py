c1cfad527f8b68fba16b15989f2939842ef1315b
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-04-30 10:15:56 +0900

    docs: Update views.py and urls.py
     비밀번호 변경 API 추가

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", users_views.Users.as_view()),
     path("me/", users_views.Me.as_view()),
     path("@<str:username>/", users_views.PublicUser.as_view()),
-    
+    path("change-password", users_views.ChangePassword.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -63,4 +63,21 @@ class PublicUser(APIView):
         serializer = PublicUserSerializer(user)
         return Response(serializer.data)
 
-            
+class ChangePassword(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(
+                status=status.HTTP_204_NO_CONTENT,
+            )
+        else:
+            raise PermissionDenied

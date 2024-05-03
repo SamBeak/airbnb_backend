692d26871c9b66ca193976f312b42a75b0fe8e25
SamBeak <114740158+SamBeak@users.noreply.github.com>
2024-05-03 16:44:22 +0900

    docs: Add category serializer
     카테고리 serializer 추가

---
+++ categories/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Category
+
+class CategorySerializer(ModelSerializer):
+    
+    class Meta:
+        model = Category
+        fields = (
+            "pk",
+            "name",
+            "kind",
+        )
